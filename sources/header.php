b2e40cad8f0225a733a1d56fe9ead7ec9ab0b517
jesperoverbeeke <jespo9@hotmail.nl>
2016-02-12 15:57:37 +0100

    Update header.php

--- sources/header.php
+++ sources/header.php
@@ -1,1 +1,13 @@
-<p>Deze pagina bevat de menu knoppen.</p>
+<style>
+img {
+    width: 100%;
+    height: 120px;
+}
+</style>
+<img src="sources/palen.jpg"  >
+<ul>
+  <li><a class="active" href="#home">Home</a></li>
+  <li><a href="#news">News</a></li>
+  <li><a href="#contact">Contact</a></li>
+  <li><a href="#about">About</a></li>
+</ul>
