60ac58cf7d50b9308853e49689892b553743ca3b
jesperoverbeeke <jespo9@hotmail.nl>
2016-03-03 14:15:29 +0100

    Update header.php

--- sources/header.php
+++ sources/header.php
@@ -6,14 +6,11 @@ img {
 	margin:0;
 	padding:0;
 }
-menu{
-	margin:0;
-	padding:0;
-}
+
 
 </style>
-<img src="/sources/palen.jpg"  >
-<ul>
+<img src="sources/strand.jpg"  >
+<ul style="margin-top: -4px;">
   <li><a class="active" class="menu" href="#home">Home</a></li>
   <li><a class="menu" href="#news">News</a></li>
   <li><a class="menu" href="#contact">Contact</a></li>
