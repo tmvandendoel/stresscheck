3bfc4494c3dc65c476fce67322038a5adba53977
jesperoverbeeke <jespo9@hotmail.nl>
2016-03-04 19:21:26 +0100

    Update default.php

--- default.php
+++ default.php
@@ -10,15 +10,40 @@
     <div id="nav">
 	<p>submenu<br>hallo<br><br><br><br><br><br>
 	</div>
-    <div id="body">
-     
-       <p> En hier dan de pagin
-	   -inhouEn hier dan de pagina-inhoud
-	   En hier dan de pagina-inhoudEn hier dan 
-	   de pagina-inhoudEn hier dan de pagina-inhoud
-	   En hier dan de pagina-inhoudEn hier dan de 
-	   pagina-inhoud
-	   d.
+    <div id="body" style="overflow-y:scroll; auto;">
+	
+     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor lacus ligula, et lobortis odio suscipit in.
+	 Vestibulum sodales vulputate urna vulputate convallis. Suspendisse eget justo non neque luctus consequat. Pellentesque
+	 sodales elementum sapien, eu ultrices nisi semper eget. Vivamus eget odio ut ex iaculis malesuada eu vitae odio. Nam eu 
+	 leo eget nisi finibus dictum eget in risus. Nam luctus turpis eu porta sagittis. Suspendisse euismod erat a mi euismod 
+	 blandit. Mauris vulputate erat dui, vitae maximus mauris interdum id. Nulla egestas molestie elementum. Proin augue odio
+	 , efficitur eget erat eget, luctus pharetra orci.
+
+Praesent vulputate sapien a turpis pharetra accumsan. Nulla lobortis dui sem, eget ultricies 
+dui ultrices eget. Fusce sodales augue augue, vel pellentesque mi tempus in. Maecenas sodales
+ fringilla auctor. Morbi scelerisque velit et odio blandit eleifend sed sed nibh. Duis aliquam 
+ augue a risus pharetra, vel suscipit enim facilisis. Praesent feugiat ultricies turpis, vel p
+ ellentesque est lobortis sit amet. Nulla sem nulla, interdum vel tempor sit amet, condimentum
+ cursus orci. Maecenas sed fermentum erat. Vivamus rhoncus accumsan sapien, eu eleifend mi volut
+ pat vitae. Integer eget ex egestas, sollicitudin turpis tempor, euismod ligula. Integer sapien r
+ isus, auctor nec lacus et, pulvinar venenatis purus. Vivamus nec lorem sapien.
+
+Maecenas ac ex interdum, rutrum lorem quis, pellentesque risus. Cras at consectetur metus, a venenat
+is neque. Praesent venenatis sapien sed felis interdum, finibus sollicitudin lectus sodales. Vestibulum
+ eleifend ex at rhoncus consectetur. Maecenas maximus euismod feugiat. Aenean vel lorem felis. Integer 
+ in sapien lectus. Curabitur maximus consectetur justo, ut vestibulum urna ornare a. Nam imperdiet mauris 
+ arcu, non faucibus elit finibus sed. Aenean consectetur fringilla nulla. Mauris rutrum magna ut suscipit eg
+ estas. Pellentesque bibendum ipsum sed arcu tristique eleifend. Duis convallis sollicitudin lectus ut congue.
+ Phasellus auctor enim eget posuere finibus.
+
+
+Vestibulum viverra at orci sit amet laoreet. Nunc sit amet facilisis nibh. Phasellus suscipit pellentesque 
+ligula id hendrerit. Nam ut eros ultricies, suscipit lorem ac, porttitor ante. Ut lacinia vehicula leo, ac iac
+ulis massa posuere vel. Nunc eget libero eget mi lobortis consectetur. Proin id risus ac leo facilisis vestibulu
+m vel at massa. Phasellus molestie egestas tellus id elementum. Vivamus faucibus est purus, ac scelerisque est moll
+is ac. Aliquam erat volutpat. Suspendisse finibus lacus ut tortor placerat sagittis. Nulla facilisi. Aliquam non do
+lor at nisl consequat mollis. Suspendisse convallis metus risus, at euismod elit tempus at. Etiam sed ante massa. N
+am molestie leo ac orci semper, id eleifend orci convallis.  
 	   </p>
     </div>
 	
